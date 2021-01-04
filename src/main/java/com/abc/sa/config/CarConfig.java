ae83bbda3409aa6f8cfb483c2c0eb87b7a9b4481
wwywmy <378297869@qq.com>
2021-01-04 20:12:51 +0800

    多实例bean管理

--- src/main/java/com/abc/sa/config/CarConfig.java
+++ src/main/java/com/abc/sa/config/CarConfig.java
@@ -2,12 +2,14 @@ package com.abc.sa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.abc.sa.bean.Car;
 
 @Configuration
 public class CarConfig {
 
+	@Scope("prototype")
 	@Bean(initMethod = "init", destroyMethod = "destroy")
 	public Car getCar() {
 		return new Car();
